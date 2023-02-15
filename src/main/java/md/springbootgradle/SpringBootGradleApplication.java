dd85aeb9f98d4d6140785be0a05525fa05d383af
MarekDudek <marek.dudek@gmail.com>
2023-02-15 22:12:05 +0100

    fresh Spring Boot application with Gradle

---
+++ src/main/java/md/springbootgradle/SpringBootGradleApplication.java
@@ -0,0 +1,15 @@
+package md.springbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md/springbootgradle/SpringBootGradleApplicationTests.java
@@ -0,0 +1,15 @@
+package md.springbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradleApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
